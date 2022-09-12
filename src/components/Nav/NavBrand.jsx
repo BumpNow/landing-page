51175fafa815e4cc1a42643ad213c65a21e0247d
JohnnyNi13 <113223783+JohnnyNi13@users.noreply.github.com>
2022-09-12 00:02:21 -0700

    Update NavBrand.jsx

--- src/components/Nav/NavBrand.jsx
+++ src/components/Nav/NavBrand.jsx
@@ -5,7 +5,7 @@ const NavBrand = ({ toggleTheme }) => {
     <div className="flex-1">
       <a className="btn btn-ghost normal-case text-3xl">
     
-        <h1 className="ml-1 lg:mb-1 lg:text-4xl">Bumpr</h1>
+        <h1 className="ml-1 lg:mb-1 lg:text-4xl"><img href="/src/assets/images/bump-logo.png"></img></h1>
       </a>
     </div>
   );
