8ca54624b6251a5a37b760a1bed323283278dd4a
JohnnyNi13 <113223783+JohnnyNi13@users.noreply.github.com>
2022-09-11 23:47:12 -0700

    Add files via upload

---
+++ src/routes/Contact.jsx
@@ -0,0 +1,5 @@
+const Contact = () => {
+  return <h1>Coming Soon... Stay tuned, thanks for being curious! Join our discord and try out app in the meantime!</h1>;
+};
+
+export default Contact;
---
+++ src/routes/FAQ.jsx
@@ -0,0 +1,5 @@
+const FAQ = () => {
+  return <h1>Coming Soon... Stay tuned, thanks for being curious! Join our discord and try out app in the meantime!</h1>;
+};
+
+export default FAQ;
